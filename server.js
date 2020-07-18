4478976141b9ece0c4b011aafc37050853e6ce9d
Jinyeong Park <jinyeongpark1205@gmail.com>
2020-07-18 02:36:39 -0700

    .

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const database = {
   users: [
     {
       id: '123',
       name: 'Jenny',
+      password: 'cookies',
       email: 'jenny@gmail.com',
       entries: 0,
       joined: new Date()
@@ -17,6 +20,7 @@ const database = {
     {
       id: '124',
       name: 'Sally',
+      password: 'banana',
       email: 'sally@gmail.com',
       entries: 0,
       joined: new Date()
@@ -45,7 +49,7 @@ app.post('/signin', (req, res) => {
 
   if (req.body.email === database.users[0].email &&
     req.body.password === database.users[0].password) {
-      res.json('success');
+      res.json(database.users[0]);
   } else {
     res.status(400).json('error logging in');
   }
